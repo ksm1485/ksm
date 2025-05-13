0984eb28435d786a99bc9617dfcf1e1b2c018166
김상명 <tkdaudtkwk@naver.com>
2025-05-13 13:54:15 +0900

    추석 추가

--- workspace_SQL/오라클.sql
+++ workspace_SQL/오라클.sql
@@ -16,5 +16,14 @@ sal*12+comm s2,
 sal*12+comm "s s"
 from emp;
 
-
-
+-- sal 컬럼을 기준으로 오름차순 - 작은 것 부터 크 것 순서로
+select * from emp order by sal asc;
+-- sal 컬럼 기준으로 내림차순 - 큰 것부터
+select * from emp order by sal desc;
+-- 가장 먼저 입사한 순서로 출력
+select * from emp order by hiredate;
+select * from emp order by ename desc;
+-- 부서번호로 오름차순, 겹치면 sal 내림차순
+select * from emp order by deptno asc, sal desc;
+-- deptno asc, sal desc;이 상황에서 연봉으면 사원번호 내림차순 정렬
+select * from emp order by deptno asc, sal desc, empno desc;
